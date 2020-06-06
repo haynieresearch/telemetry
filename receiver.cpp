689a5bb318302f55d6210dfb5b2840445d70b00c
Lance Haynie <lance@hayniemail.com>
2020-06-05 23:21:32 -0600

    updates

--- receiver.cpp
+++ receiver.cpp
@@ -24,16 +24,10 @@
 
 //program setup
 void setup() {
-	//let the system boot real quick
 	delay(500);
-
-	//initialize serial
 	initSerial();
-
-	//startup the lcd
 	lcd.begin(20, 4);
 
-	//show a quick splash screen on the lcd
 	splash();
 	delay(5000);
 }
